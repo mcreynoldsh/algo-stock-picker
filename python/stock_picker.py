e425b4ea5468fd9330a1ed0b369ece5935f87f3f
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-06-06 12:01:27 -0500

    initial

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,16 @@
 def picker(prices):
-    pass
+    rest_of_prices = prices.copy()
+    largest_profit = 0
+    first_index = 0
+    second_index= 0
+    for price in prices:
+        rest_of_prices.remove(price)
+        for comp_price in rest_of_prices:
+            if comp_price - price > largest_profit:
+                largest_profit= comp_price - price
+                print(prices)
+                print(rest_of_prices)
+                first_index= prices.index(price)
+                second_index = prices.index(comp_price)
+
+    return [first_index,second_index]
